c449de47efe487965f7fa99d8cf11478d9fc855a
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-11-27 16:31:59 +0200

    Models changed according to changes on the backend.

--- lib/src/model/mobile/login_mobile_info.dart
+++ lib/src/model/mobile/login_mobile_info.dart
@@ -1,13 +1,15 @@
 import 'package:thingsboard_client/thingsboard_client.dart'
-    show OAuth2ClientInfo, VersionInfo;
+    show OAuth2ClientInfo, VersionInfo, StoreInfo;
 
 class LoginMobileInfo {
   const LoginMobileInfo({
     required this.oAuth2Clients,
+    required this.storeInfo,
     required this.versionInfo,
   });
 
   final List<OAuth2ClientInfo> oAuth2Clients;
+  final StoreInfo? storeInfo;
   final VersionInfo? versionInfo;
 
   factory LoginMobileInfo.fromJson(Map<String, dynamic> json) {
@@ -15,6 +17,9 @@ class LoginMobileInfo {
       oAuth2Clients: json['oAuth2ClientLoginInfos']
           .map<OAuth2ClientInfo>((e) => OAuth2ClientInfo.fromJson(e))
           .toList(),
+      storeInfo: json['storeInfo'] != null
+          ? StoreInfo.fromJson(json['storeInfo'])
+          : null,
       versionInfo: json['versionInfo'] != null
           ? VersionInfo.fromJson(json['versionInfo'])
           : null,
--- lib/src/model/mobile/version_info.dart
+++ lib/src/model/mobile/version_info.dart
@@ -1,19 +1,28 @@
-import 'mobile_models.dart' show StoreInfo, MobileVersionInfo;
+import 'package:thingsboard_client/src/model/model.dart' show PlatformVersion;
 
 class VersionInfo {
-  const VersionInfo({required this.storeInfo, required this.mobileVersionInfo});
+  const VersionInfo({
+    required this.minVersion,
+    required this.minVersionReleaseNotes,
+    required this.latestVersion,
+    required this.latestVersionReleaseNotes,
+  });
 
   factory VersionInfo.fromJson(Map<String, dynamic> json) {
     return VersionInfo(
-      storeInfo: json['storeInfo'] != null
-          ? StoreInfo.fromJson(json['storeInfo'])
+      minVersion: json['minVersion'] != ''
+          ? PlatformVersion.fromString(json['minVersion'])
           : null,
-      mobileVersionInfo: json['mobileAppVersionInfo'] != null
-          ? MobileVersionInfo.fromJson(json['mobileAppVersionInfo'])
+      minVersionReleaseNotes: json['minVersionReleaseNotes'],
+      latestVersion: json['latestVersion'] != ''
+          ? PlatformVersion.fromString(json['latestVersion'])
           : null,
+      latestVersionReleaseNotes: json['latestVersionReleaseNotes'],
     );
   }
 
-  final StoreInfo? storeInfo;
-  final MobileVersionInfo? mobileVersionInfo;
+  final PlatformVersion? minVersion;
+  final String minVersionReleaseNotes;
+  final PlatformVersion? latestVersion;
+  final String latestVersionReleaseNotes;
 }
